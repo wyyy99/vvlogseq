92ebf4d7654ae29e66cf105f0706d6465ef50b2c
wyyy99 <3133078280@qq.com>
2023-09-27 12:18:57 +0800

    auto save

--- vvlogseqAutoSave.bat
+++ vvlogseqAutoSave.bat
@@ -1,7 +1,10 @@
-#!bin/bin/bash
+start "C:\Windows\System32\cmd.exe"
+E:
 cd E:\vvlogseq
 git init
 git add .
 git commit -m "auto save"
 git pull vvlogseq-origin main
 git push -u vvlogseq-origin HEAD:main
+pause
+taskkill /f /im cmd.exe
