6b8190d252d9affea51626efb038b3f4d300ed94
wyyy99 <3133078280@qq.com>
2023-09-27 12:39:27 +0800

    auto save

--- vvlogseqAutoSave.bat
+++ vvlogseqAutoSave.bat
@@ -1,3 +1,6 @@
+@echo off 
+@title bat execute git auto commit #运行时命令行窗口的 title
+
 E:
 cd E:\vvlogseq
 git init
@@ -5,5 +8,3 @@ git add .
 git commit -m "auto save"
 git pull vvlogseq-origin main
 git push -u vvlogseq-origin HEAD:main
-pause
-taskkill /f /im cmd.exe
